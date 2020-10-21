ca4c0cfe4b1867e3f98b74a89dfb3f99422176e2
morgan <msswanso@calpoly.edu>
2020-10-20 17:53:42 -0700

    Added BinaryNet Tutorial files

--- ftbnn/binarynet.py
+++ ftbnn/binarynet.py
@@ -1,1 +1,95 @@
 import tensorflow as tf
+import larq as lq
+import numpy as np
+import matplotlib.pyplot as plt
+
+def preprocess_data():
+    num_classes = 10
+
+    (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
+
+    train_images = train_images.reshape((50000, 32, 32, 3)).astype("float32")
+    test_images = test_images.reshape((10000, 32, 32, 3)).astype("float32")
+
+    # Normalize pixel values to be between -1 and 1
+    train_images, test_images = train_images / 127.5 - 1, test_images / 127.5 - 1
+
+    train_labels = tf.keras.utils.to_categorical(train_labels, num_classes)
+    test_labels = tf.keras.utils.to_categorical(test_labels, num_classes)
+    return ((train_images, train_labels), (test_images, test_labels))
+
+def build_model():
+    # All quantized layers except the first will use the same options
+    kwargs = dict(input_quantizer="ste_sign",
+                  kernel_quantizer="ste_sign",
+                  kernel_constraint="weight_clip",
+                  use_bias=False)
+    return tf.keras.models.Sequential([
+        # In the first layer we only quantize the weights and not the input
+        lq.layers.QuantConv2D(128, 3,
+                              kernel_quantizer="ste_sign",
+                              kernel_constraint="weight_clip",
+                              use_bias=False,
+                              input_shape=(32, 32, 3)),
+        tf.keras.layers.BatchNormalization(momentum=0.999, scale=False),
+
+        lq.layers.QuantConv2D(128, 3, padding="same", **kwargs),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+        tf.keras.layers.BatchNormalization(momentum=0.999, scale=False),
+
+        lq.layers.QuantConv2D(256, 3, padding="same", **kwargs),
+        tf.keras.layers.BatchNormalization(momentum=0.999, scale=False),
+
+        lq.layers.QuantConv2D(256, 3, padding="same", **kwargs),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+        tf.keras.layers.BatchNormalization(momentum=0.999, scale=False),
+
+        lq.layers.QuantConv2D(512, 3, padding="same", **kwargs),
+        tf.keras.layers.BatchNormalization(momentum=0.999, scale=False),
+
+        lq.layers.QuantConv2D(512, 3, padding="same", **kwargs),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+        tf.keras.layers.BatchNormalization(momentum=0.999, scale=False),
+        tf.keras.layers.Flatten(),
+
+        lq.layers.QuantDense(1024, **kwargs),
+        tf.keras.layers.BatchNormalization(momentum=0.999, scale=False),
+
+        lq.layers.QuantDense(1024, **kwargs),
+        tf.keras.layers.BatchNormalization(momentum=0.999, scale=False),
+
+        lq.layers.QuantDense(10, **kwargs),
+        tf.keras.layers.BatchNormalization(momentum=0.999, scale=False),
+        tf.keras.layers.Activation("softmax")
+    ])
+
+def train_model(data, model):
+    (train_images, train_labels), (test_images, test_labels) = data
+    model.compile(
+        tf.keras.optimizers.Adam(lr=0.01, decay=0.0001),
+        loss="categorical_crossentropy",
+        metrics=["accuracy"],
+    )
+    return model.fit(
+        train_images,
+        train_labels,
+        batch_size=50,
+        epochs=10,
+        validation_data=(test_images, test_labels),
+        shuffle=True
+    )
+
+def plot_results(trained_model):
+    plt.plot(trained_model.history['acc'])
+    plt.plot(trained_model.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+
+    print(np.max(trained_model.history['acc']))
+    print(np.max(trained_model.history['val_acc']))
+
+
+if __name__ == "__main__":
+    plot_results(train_model(preprocess_data(), build_model()))
--- test/binarynet.py
+++ test/binarynet.py
@@ -5,5 +5,17 @@ class TestBinaryNet(unittest.TestCase):
     def test_import(self):
         from ftbnn import binarynet
 
+    def test_import_data(self):
+        from ftbnn import binarynet
+        binarynet.preprocess_data()
+
+    def test_build_model(self):
+        from ftbnn import binarynet
+        binarynet.build_model()
+
+    def test_train_cifar10(self):
+        from ftbnn.binarynet import plot_results, train_model, preprocess_data, build_model
+        plot_results(train_model(preprocess_data(), build_model()))
+
 if __name__ == '__main__':
     unittest.main()
