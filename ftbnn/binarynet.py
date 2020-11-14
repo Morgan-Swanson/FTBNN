391aedbc8a9a59a2d53db944dca695353de55d31
Tristan Chutka <tristan.chutka@outlook.com>
2020-11-14 13:09:39 -0800

    preliminary gan support (dcgan) Note: non functional, depends proper data sizes

--- ftbnn/binarynet.py
+++ ftbnn/binarynet.py
@@ -2,6 +2,18 @@ import tensorflow as tf
 import larq as lq
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+from IPython import display
+
+EPOCHS = 10
+noise_dim = 100
+num_examples_to_generate = 16
+
+#static seed
+seed = tf.random.normal([num_examples_to_generate, noise_dim])
+
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
 def preprocess_data():
     num_classes = 10
@@ -74,10 +86,102 @@ def train_model(data, model):
         train_images,
         train_labels,
         batch_size=50,
-        epochs=10,
+        epochs=EPOCHS,
         validation_data=(test_images, test_labels),
         shuffle=True
     )
+
+def train_gan(data, epochs, discriminator, generator):
+
+    for epoch in range(epochs):
+        start = time.time()
+
+        train_step(data, discriminator, generator)
+
+        predictions = generator(seed, training=False)
+
+        show_image(predictions)
+
+        print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+
+    # Generate after the final epoch
+    show_image(predictions)
+
+@tf.function
+def train_step(images, discriminator, generator):
+    noise = tf.random.normal([50000, noise_dim])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+
+def show_image(predictions):
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+
+    display.clear_output(wait=True)
+    plt.show()
+
+
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Dense(8*8*256, use_bias=False, input_shape=(100,)))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.LeakyReLU())
+
+    model.add(tf.keras.layers.Reshape((8, 8, 256)))
+    assert model.output_shape == (None, 8, 8, 256) # Note: None is the batch size
+
+    model.add(tf.keras.layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 8, 8, 128)
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.LeakyReLU())
+
+    model.add(tf.keras.layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 16, 16, 64)
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.LeakyReLU())
+
+    # model.add(tf.keras.layers.Conv2DTranspose(16, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    # assert model.output_shape == (None, 32, 32, 16)
+    # model.add(tf.keras.layers.BatchNormalization())
+    # model.add(tf.keras.layers.LeakyReLU())
+
+    model.add(tf.keras.layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    print(model.output_shape)
+    assert model.output_shape == (None, 32, 32, 1)
+
+    return model
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', input_shape=[32, 32, 1]))
+    model.add(tf.keras.layers.LeakyReLU())
+    model.add(tf.keras.layers.Dropout(0.3))
+
+    model.add(tf.keras.layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(tf.keras.layers.LeakyReLU())
+    model.add(tf.keras.layers.Dropout(0.3))
+
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(1))
+
+    return model
 
 def plot_results(trained_model):
     plt.plot(trained_model.history['accuracy'])
@@ -101,12 +205,42 @@ def save_model(trained_model):
         options=None,
     )
 
+def discriminator_loss(real_output, fake_output):
+    real_loss = tf.keras.losses.BinaryCrossentropy(tf.ones_like(real_output), real_output)
+    fake_loss = tf.keras.losses.BinaryCrossentropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    return tf.keras.losses.BinaryCrossentropy(tf.ones_like(fake_output), fake_output)
+
 if __name__ == "__main__":
     #required to run on some systems
     config = tf.compat.v1.ConfigProto()
     config.gpu_options.allow_growth = True
     session = tf.compat.v1.Session(config=config)
-    model = build_model()
-    save_model(model)
-    trained_model = train_model(preprocess_data(), model)
-    plot_results(trained_model)
+
+    #init random noise    
+    noise = tf.random.normal([1, 100])
+
+    #init models
+    generator = make_generator_model()
+    discriminator = build_model()
+
+    (train_images, train_labels), (test_images, test_labels) = preprocess_data()
+    
+    train_gan(train_images, EPOCHS, discriminator, generator)
+
+
+    # generated_image = generator(noise, training=False)
+    # decision = discriminator(generated_image)
+    # print (decision)
+
+    
+
+
+
+    # model = build_model()
+    # save_model(model)
+    # trained_model = train_model(data, model)
+    # plot_results(trained_model)
